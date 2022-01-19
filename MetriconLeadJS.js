3381c14bca981de83a370d52752bf46d5f82fe01
Sanjaya Yapa <sanjaya.yapa@velrada.com>
2022-01-19 12:06:12 +1100

    Update to the Metricon Lead JS

--- MetriconLeadJS.js
+++ MetriconLeadJS.js
@@ -1,50 +1,51 @@
-function CallNurtureProcess(recordid){
+// This JS Function is used to call a flow from a ribbon button
+// Use the ribbon button to pass the primary control CRM Parameter
+// Uses the XRM WebApi to retrieve the Environment Variable
+// Environment variable stores the URL of the flow
+// The flow trigger should be HTTP request of type POST
+
+function CallNurtureProcess(primaryControl) {
     var isConfirmed = confirm("Are you sure you want to continue?");
-    alert(recordid);
-    if(isConfirmed){
-        var envVarDefQuery = "environmentvariabledefinitions?$filter=schemaname eq 's365_NurtureProcessURL'&$select=environmentvariabledefinitionid";
-        var envVarDef = ExecuteFetch(envVarDefQuery);
-        if(envVarDef.value[0] !== null && envVarDef.value[0] !== ''){
-            var envVarValQuery = "environmentvariablevalues?$filter=_environmentvariabledefinitionid_value eq '" + envVarDef.value[0].environmentvariabledefinitionid + "'&$select=value";
-			var envVarVal = Downer.Project.ExecuteFetch(envVarValQuery); 
+    var envVarDef = "";
+    var envVarVal = "";
 
-            if(envVarVal.value[0] !== null && envVarVal.value[0] !== ''){
-                var request = new XMLHttpRequest();
-                var url = envVarVal.value[0].value;
-                request.open("POST",url,true);
-                request.setRequestHeader('Content-Type','application/json');
-                request.send(JSON.stringify(recordid[0]));
-            }
-        }
-    }
-}
+    var parameterId = JSON.stringify({
+        "id": primaryControl.data.entity.getId().replace("{", "").replace("}", "")
+    });
 
-function ExecuteFetch(query){
-    var result;
-
-    var serverUrl = Xrm.Utility.getGlobalContext().getClientUrl();
-    var request = new XMLHttpRequest();
-    
-    request.open("GET",serverUrl + "api/data/v9.1/" + query, false);
-    request.setRequestHeader("Accept", "application/json");
-	request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-	request.setRequestHeader("OData-MaxVersion", "4.0");
-	request.setRequestHeader("OData-Version", "4.0");
-    request.onreadystatechange = function(){
-        if(this.readyState === 4){
-            request.onreadystatechange = null;
-            if(this.status === 200){
-                var data = JSON.parse(this.respose);
-                if(data != null){
-                    result = data;
+    if (isConfirmed) {
+        Xrm.WebApi.retrieveMultipleRecords("environmentvariabledefinition", "?$select=environmentvariabledefinitionid&$filter=schemaname eq 's365_NurtureProcessURL'").then(
+            function success(result) {
+                for (var x = 0; x < result.entities.length; x++) {
+                    var resutlSet = result.entities[0];
+                    if (resutlSet['environmentvariabledefinitionid'] != null) {
+                        envVarDef = resutlSet['environmentvariabledefinitionid'];
+                        Xrm.WebApi.retrieveMultipleRecords('environmentvariablevalue', "?$select=value&$filter=_environmentvariabledefinitionid_value eq '" + envVarDef + "'").then(
+                            function success(result) {
+                                for (var x = 0; x < result.entities.length; x++) {
+                                    var resultSet = result.entities[0];
+                                    if (resultSet['value'] != null) {
+                                        envVarVal = resultSet['value'];
+                                        if (envVarVal !== null && envVarVal !== '') {
+                                            var request = new XMLHttpRequest();
+                                            var url = envVarVal;
+                                            request.open("POST", url, true);
+                                            request.setRequestHeader('Content-Type', 'application/json');
+                                            request.send(parameterId);
+                                            console.log('Completed');
+                                        }
+                                    }
+                                }
+                            },
+                            function(error) {
+                                console.log('ERROR - Env Var Value: ' + error.message);
+                            }
+                        );
+                    }
                 }
-            }
-            else{
-                var error = JSON.parse(this.response).error;
-                alert(error.message);
-            }
-        }
-    };
-    request.send();
-    return result;
+            },
+            function(error) {
+                console.log('ERROR: ' + error.message);
+            });
+    }
 }
